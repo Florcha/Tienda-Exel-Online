9938de01e5ef4dc933e6830d5c549e4b7caee455
Florencia <mflorenciacarranza@gmail.com>
2022-04-19 21:14:40 -0300

    Instalación de Tailwind y DaisyUI

--- src/App.js
+++ src/App.js
@@ -2,23 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const styleP = {
+    padding: '15px',
+    border: 'solid 5px red'
+  }
+
+  const styles = {
+    header: 'text-3xl font-light underline text-amber-600'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{color:'pink'}}>Bienvenidos a la tienda Online de EXEL!</h1>
+      <li>Facial</li>
+      <li>Corporal</li>
+      <li>Cabello</li>
+      <li>{ 3+1 }</li>
+      <p style={styleP}>Página en construcción, gracias por la espera, será sorprendente!</p>
+      <h1 className={styles.header}>
+        Hola, estás en EXELBIOCOSMÉTICA
+      </h1>
+      <button class="btn btn-primary">One</button>
+      <button class="btn btn-secondary">Two</button>
+      <button class="btn btn-accent btn-outline">Three</button>
+    </>
   );
 }
 
