fde81d7bef3b76f64639c3a84c1a68305d72a130
Florencia <mflorenciacarranza@gmail.com>
2022-05-25 11:40:22 -0300

    Incluyo en index el jsonToFirestore

--- src/components/cart/ProductCard.jsx
+++ src/components/cart/ProductCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const ProductCard = ({ product }) => {
   return (
     <Link to={'/product/' + product.id}>
-      <div style={{ border: 'solid black 5px', margin: '20px', padding: '20px', fontFamily: 'white' }}>
+      <div style={{ border: 'solid black 1px', margin: '20px', padding: '20px', fontFamily: 'white' }}>
         <img src={product.pictureUrl} alt={product.productName} style={{ width: '150px', height: '150px'}}/>
         <strong>{product.productName}</strong>
         <div>{product.mark}</div>
