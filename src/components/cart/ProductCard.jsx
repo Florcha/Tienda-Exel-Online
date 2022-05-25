2d580dc3fe4e841170c7f876128ee1f4f195b2e8
Florencia <mflorenciacarranza@gmail.com>
2022-05-25 11:08:34 -0300

    Modifico ProductCard y Products

--- src/components/cart/ProductCard.jsx
+++ src/components/cart/ProductCard.jsx
@@ -5,7 +5,8 @@ const ProductCard = ({ product }) => {
   return (
     <Link to={'/product/' + product.id}>
       <div style={{ border: 'solid black 5px', margin: '20px', padding: '20px', fontFamily: 'white' }}>
-        <strong>{product.name}</strong>
+        <img src={product.pictureUrl} alt={product.productName} style={{ width: '150px', height: '150px'}}/>
+        <strong>{product.productName}</strong>
         <div>{product.mark}</div>
       </div>
     </Link>
--- src/data/products.js
+++ src/data/products.js
@@ -1,47 +1,46 @@
 export const products = [
     {
-        id: 1, 
-        productName:'Gel', 
-        mark:'Exel',
-        category: 'Facial',
-        price: 1100,
-        stock:'150',
-        pictureUrl: 
+      id: 1,
+      productName: 'Gel',
+      mark: 'Exel',
+      category: 'Facial',
+      price: 1100,
+      stock: '150',
+      pictureUrl:
         'https://www.heavenimagenes.com/heavencommerce/cbed7736-4c76-4fd0-b719-9ff071142423/images/v2/EXEL/2007151124473576_01_medium.jpg'
     },
-
+  
     {
-        id: 2, 
-        productName:'Crema', 
-        mark:'Lidherma',
-        category: 'Corporal',
-        price: 2900,
-        stock:'300',
-        pictureUrl: 
+      id: 2,
+      productName: 'Crema',
+      mark: 'Lidherma',
+      category: 'Corporal',
+      price: 2900,
+      stock: '300',
+      pictureUrl:
         'https://www.lidherma.com/thumbnails/ldh_skinbioma_facecream_450x450.jpg'
     },
-
+  
     {
-        id: 3, 
-        productName:'Enjuague', 
-        mark:'Loreal',
-        category: 'Cabello',
-        price: 1200,
-        stock:'100',
-        pictureUrl: 
+      id: 3,
+      productName: 'Enjuague',
+      mark: 'Loreal',
+      category: 'Cabello',
+      price: 1200,
+      stock: '100',
+      pictureUrl:
         'https://http2.mlstatic.com/D_NQ_NP_613395-MLA32318582398_092019-W.jpg'
     },
-
-    {  
-        id: 4, 
-        productName:'Shampoo', 
-        mark:'Nexxus',
-        category: 'Cabello',
-        price: 1900,
-        stock:'210',
-        pictureUrl: 
+  
+    {
+      id: 4,
+      productName: 'Shampoo',
+      mark: 'Nexxus',
+      category: 'Cabello',
+      price: 1900,
+      stock: '210',
+      pictureUrl:
         'https://images.ulta.com/is/image/Ulta/2049094?op_sharpen=1&resMode=bilin&qlt=85&wid=800&hei=800&fmt=jpg'
     },
 ];
-
-
+  
