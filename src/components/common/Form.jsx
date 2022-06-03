fb8147166493f96659f6ffad51e48c74e2fc5771
Florencia <mflorenciacarranza@gmail.com>
2022-06-03 19:57:29 -0300

    Cambio h1 y h4 de Form

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -35,8 +35,8 @@ export default function Form() {
         <div className="contact_form">
 
           <div className="formulario">
-            <h1>Formulario de contacto</h1>
-            <h4>Escríbenos y en breve los pondremos en contacto contigo</h4>
+            <h1>Formulario de Contacto</h1>
+            <h4>Escríbenos y en breve nos pondremos en contacto</h4>
 
             <form onSubmit={d => onSubmit(d)}>
               <p>
